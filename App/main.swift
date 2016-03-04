c936577f2d822c608fdbb19679c67246b3d34856
Caesar Wirth <cjwirth@gmail.com>
2016-03-04 10:14:40 +0900

    Got rid of code so i can type it in later

--- App/main.swift
+++ App/main.swift
@@ -1,42 +1,31 @@
 import Nest
 import Curassow
 
+let router = Router()
+
+router.get("/") { _ in
+    return Response(status: .OK, body: "Hello, World!")
+}
+
+
+
+
+
+
+let server = router.handle
+serve(server)
+
+// Data
+
 var pokedex: [Pokemon] = [
+    // フシギダネ
     Pokemon(name: "Bulbasaur", description: "Bulbasaur can be seen napping in bright sunlight."),
+    // ヒトカゲ
     Pokemon(name: "Charmander", description: "The flame that burns at the tip of its tail is an indication of its emotions."),
+    // ゼニガメ
     Pokemon(name: "Squirtle", description: "Squirtle's shell is not merely used for protection.")
 ]
 
-var trainers: [Trainer] = [
-    Trainer(name: "Tom Oliver", memo: "日本語、お上手ですね")
+let trainers: [Trainer] = [
+    Trainer(name: "Tom Oliver", memo: "日本語、上手ですね✌️")
 ]
-
-let router = Router()
-let server = router.handle
-
-router.get("/pokemon") { request in
-    let pokemonJson = try pokedex.jsonString()
-    return Response(status: .OK, body: pokemonJson)
-}
-
-router.post("/pokemon") { request in
-    guard let json = request.body else {
-        return Error.BadRequest
-    }
-
-    let pokemon: Pokemon
-    do {
-        if let deserialized = try Pokemon.deserialize(json).first {
-            pokemon = deserialized
-        } else {
-            return Error.BadRequest
-        }
-    } catch {
-        return Error.BadRequest
-    }
-
-    pokedex.append(pokemon)
-    return Response(status: .OK, body: try pokemon.jsonString())
-}
-
-serve(server)
