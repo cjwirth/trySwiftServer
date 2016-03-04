ba113c587a7a1c8531ac03d62bd3824de99a4f06
Caesar Wirth <cjwirth@gmail.com>
2016-03-04 10:50:53 +0900

    Added pokemon

--- App/main.swift
+++ App/main.swift
@@ -1,24 +1,8 @@
 import Nest
 import Curassow
 
-let router = Router()
-
-router.get("/") { _ in
-    return Response(status: .OK, body: "Hello, World!")
-}
-
-
-
-
-
-
-let server = router.handle
-serve(server)
-
-// Data
-
 var pokedex: [Pokemon] = [
-    // フシギダネ
+                             // フシギダネ
     Pokemon(name: "Bulbasaur", description: "Bulbasaur can be seen napping in bright sunlight."),
     // ヒトカゲ
     Pokemon(name: "Charmander", description: "The flame that burns at the tip of its tail is an indication of its emotions."),
@@ -26,6 +10,38 @@ var pokedex: [Pokemon] = [
     Pokemon(name: "Squirtle", description: "Squirtle's shell is not merely used for protection.")
 ]
 
-let trainers: [Trainer] = [
-    Trainer(name: "Tom Oliver", memo: "日本語、上手ですね✌️")
-]
+let router = Router()
+
+router.get("/") { _ in
+    return Response(status: .OK, body: "Hello, World!")
+}
+
+router.get("/pokemon") { request in
+    let pokemonJson = try pokedex.jsonString()
+    return Response(status: .OK, body: pokemonJson)
+}
+
+router.post("/pokemon") { request in
+    guard let json = request.body else {
+        return Error.BadRequest
+    }
+
+    let pokemon: Pokemon
+    do {
+        if let deserialize = try Pokemon.deserialize(json).first {
+            pokemon = deserialize
+        } else {
+            return Error.BadRequest
+        }
+    } catch {
+        return Error.BadRequest
+    }
+
+    pokedex.append(pokemon)
+    return Response(status: .OK)
+}
+
+
+
+let server = router.handle
+serve(server)
